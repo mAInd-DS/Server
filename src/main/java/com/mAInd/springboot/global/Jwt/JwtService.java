216a8857669ff34c00d81f088a5af6a17c122a78
ji-vvon <jjw9895@duksung.ac.kr>
2023-10-11 00:50:55 +0900

    feat #3 [로ê¸그인] withClaim을 통한 token에 회원정보 전달

--- src/main/java/com/mAInd/springboot/global/Jwt/JwtService.java
+++ src/main/java/com/mAInd/springboot/global/Jwt/JwtService.java
@@ -1,10 +1,14 @@
 package com.mAInd.springboot.global.Jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.mAInd.springboot.domain.user.entity.Role;
+import com.mAInd.springboot.domain.user.entity.UserStatus;
+import com.mAInd.springboot.domain.user.entity.Users;
 import com.mAInd.springboot.domain.user.repository.UserRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.h2.engine.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -41,6 +45,12 @@ public class JwtService {
     private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
     private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
     private static final String EMAIL_CLAIM = "email";
+
+    private static final String NAME_CLAIM = "name";
+
+    private static final String ROLE_CLAIM = "role";
+
+    private static final String STATUS_CLAIM = "status";
     private static final String BEARER = "Bearer ";
 
     private final UserRepository userRepository;
@@ -50,6 +60,21 @@ public class JwtService {
      */
     public String createAccessToken(String email) {
         Date now = new Date();
+
+        String name = null;
+        String role = null;
+        String status = null;
+
+        Optional<Users> findUser = userRepository.findByEmail(email);
+        if(findUser.isPresent()){
+            name = findUser.get().getName();
+            role = String.valueOf(findUser.get().getRole());
+            status = String.valueOf(findUser.get().getUserStatus());
+            log.info("name: {} role: {} status: {}", name, role, status);
+        }else{
+            log.info("user 정보를 불러오지 못함");
+        }
+
         return JWT.create() // JWT 토큰을 생성하는 빌더 반환
                 .withSubject(ACCESS_TOKEN_SUBJECT) // JWT의 Subject 지정 -> AccessToken이므로 AccessToken
                 .withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod)) // 토큰 만료 시간 설정
@@ -58,6 +83,11 @@ public class JwtService {
                 //추가적으로 식별자나, 이름 등의 정보를 더 추가해도 됨
                 //추가할 경우 .withClaim(클래임 이름, 클래임 값) 으로 설정
                 .withClaim(EMAIL_CLAIM, email)
+
+                .withClaim(NAME_CLAIM, name)
+                .withClaim(ROLE_CLAIM, role)
+                .withClaim(STATUS_CLAIM, status)
+
                 .sign(Algorithm.HMAC512(secretKey)); // HMAC512 알고리즘 사용, application.properties에서 지정한 secret 키로 암호화
     }
 
