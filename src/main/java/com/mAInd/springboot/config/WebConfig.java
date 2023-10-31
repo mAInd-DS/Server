c793972bfbc20081d49e23818649f9e3b3b9f191
ji-vvon <jjw9895@duksung.ac.kr>
2023-10-31 10:54:03 +0900

    feat #8#3 [로그인] cors 설정 수정

--- src/main/java/com/mAInd/springboot/config/WebConfig.java
+++ src/main/java/com/mAInd/springboot/config/WebConfig.java
@@ -20,9 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://maind.site:8080",
+                .allowedOrigins("http://localhost:8080",
+                        "http://maind.site:8080",
+                        "https://maind.site",
+                        "http://maind.site:3000",
                         "http://localhost:3000",
-                        "https://localhost:3000",
                         "https://127.0.0.1:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
